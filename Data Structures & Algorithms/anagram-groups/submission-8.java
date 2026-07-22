0791fdcda4325dda10babb56253479dda3e6e402
anil-k-m <162057931+anil-k-m@users.noreply.github.com>
2026-07-21 23:18:24 -0400

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> output = new HashMap<>();
+        for (String s : strs) {
+            char[] charCounts = new char[26];
+            for (char c: s.toCharArray()) {
+                charCounts[c - 'a']++;
+            }
+            String key = new String(charCounts);
+            output.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(output.values());
+    }
+}
